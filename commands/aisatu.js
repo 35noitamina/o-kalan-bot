a1f3bf1005299858a9e454145dabc5fce133192a
gachao <134592219+35noitamina@users.noreply.github.com>
2025-12-22 23:17:01 +0900

    Update aisatu.js

--- commands/aisatu.js
+++ commands/aisatu.js
@@ -1,10 +1,10 @@
 import { SlashCommandBuilder } from 'discord.js';
 export default {
-  data: new SlashCommandBuilder()
-    .setName('invite')
-    .setDescription('招待リンクを作成します。'),
-  async execute(interaction) {
-    const invite = await interaction.channel.createInvite();
-    await interaction.reply(invite.url);
-  }
+  data: new SlashCommandBuilder()
+    .setName('invite')
+    .setDescription('招待リンクを作成します。'),
+  async execute(interaction) {
+    const invite = await interaction.channel.createInvite();
+    await interaction.reply(invite.url);
+  }
 };
